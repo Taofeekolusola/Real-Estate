6a122aa4dd8684a4be1a210fa5e1313fa43ef61d
Taofeek Olusola <olusolataofeek@gmail.com>
2025-09-10 10:35:51 +0100

    reset-password corrected

--- client/app/reset-password/page.tsx
+++ client/app/reset-password/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+export const dynamic = "force-dynamic"
 import type React from "react"
 import { useState, useEffect } from "react"
 import { useRouter, useSearchParams } from "next/navigation"
